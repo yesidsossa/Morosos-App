8745f0d34fdf827ded47e4b31c97714a52522acf
Alejandro Alzate Zapata <aalzatez@sura.com.co>
2019-08-20 18:07:46 -0500

    Commit inicial

---
+++ MorososApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MorososApp
+//
+//  Created by Alejandro Alzate Zapata on 8/20/19.
+//  Copyright © 2019 Morosos App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
