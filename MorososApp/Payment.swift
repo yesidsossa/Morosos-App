66c50bcc42207a57c885ff91506cbece83380eb4
Yesid Hernandez <Steven.uni21@gmail.com>
2019-08-28 16:52:31 -0500

    Recaudos

---
+++ MorososApp/Payment.swift
@@ -0,0 +1,13 @@
+//
+//  Payment.swift
+//  MorososApp
+//
+//  Created by Yesid Steven Hernandez Sossa on 8/28/19.
+//  Copyright © 2019 Morosos App. All rights reserved.
+//
+
+import UIKit
+
+class Payment: NSObject {
+
+}
